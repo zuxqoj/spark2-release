1a9ea09d640b0a795b253f2701304e60ef0d22ee
Steve Loughran <stevel@hortonworks.com>
2016-07-19 16:48:54 +0100

    SPARK-7481: pull in latest version of spark cloud module being done against master (commit #cb07c1d7), fix pom, retest. This module is only included on a -Phadoop-2.7 profile

---
+++ cloud/src/test/scala/org/apache/spark/cloud/azure/AzureTestSetup.scala
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.cloud.azure
+
+import java.net.URI
+
+import org.apache.hadoop.fs.FileSystem
+
+import org.apache.spark.cloud.CloudSuite
+
+/**
+ * Trait for Azure tests. Because no Azure-specific API calls are made, this test suite
+ * will compile against Hadoop versions which lack the hadoop-azure module. However, it
+ * won't run.
+ */
+private[cloud] trait AzureTestSetup extends CloudSuite {
+
+  override def enabled: Boolean =  {
+    super.enabled &&
+        azureFsOnClasspath &&
+        conf.getBoolean(AZURE_TESTS_ENABLED, false)
+  }
+
+  def initFS(): FileSystem = {
+    val uri = new URI(requiredOption(AZURE_TEST_URI))
+    logDebug(s"Executing Azure tests against $uri")
+    createFilesystem(uri)
+  }
+  
+  def azureFsOnClasspath: Boolean = {
+    null != getClass.getClassLoader.getResource("org/apache/hadoop/fs/azure/AzureException.class")
+  }
+}
