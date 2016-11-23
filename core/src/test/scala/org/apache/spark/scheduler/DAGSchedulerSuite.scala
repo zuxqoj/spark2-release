44b675ddc5344ca6ec2da99bfeae96a6f0c780e5
Mingjie Tang <mtang@HW12398.local>
2016-11-23 15:43:55 -0800

    DAGSchedulerSuite conflict reslove when backport
    Change-Id: I0d10378000ef1d84d5bd8abc659ca90384dcdfb9

--- core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
@@ -2089,12 +2089,3 @@ object DAGSchedulerSuite {
   def makeBlockManagerId(host: String): BlockManagerId =
     BlockManagerId("exec-" + host, host, 12345)
 }
-
-
-object DAGSchedulerSuite {
-  def makeMapStatus(host: String, reduces: Int, sizes: Byte = 2): MapStatus =
-    MapStatus(makeBlockManagerId(host), Array.fill[Long](reduces)(sizes))
-
-  def makeBlockManagerId(host: String): BlockManagerId =
-    BlockManagerId("exec-" + host, host, 12345)
-}
